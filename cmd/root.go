12734f7b02bab566de9510070c7e1c7aa6f833f6
abdullah134 <abdullahaqeel184@gmail.com>
2026-01-03 16:01:49 +0500

    Fix the error for no service found in windows

--- cmd/root.go
+++ cmd/root.go
@@ -185,6 +185,9 @@ func runRoot(cmd *cobra.Command, args []string) error {
 	}
 
 	pids, err := target.Resolve(t)
+	if err == nil && len(pids) == 0 {
+		err = fmt.Errorf("no matching process found")
+	}
 	if err != nil {
 		errStr := err.Error()
 		var errorMsg string
